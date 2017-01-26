fd6c6beca1b61220868788cfe6ca34745a8ddc46
John D. Bower <jdbower0525@gmail.com>
2017-01-26 12:27:48 -0500

    working on fields

--- NightlyApp/serializers.py
+++ NightlyApp/serializers.py
@@ -6,13 +6,13 @@ class DaySerializer(serializers.ModelSerializer):
     class Meta:
         model = Day
         fields = ('url', 'date', 'food_sales', 'beverage_sales',
-                  'liquor_sales', 'beer_sales', 'wine_sales', 'net sales',
-                  'forecast', 'last year', 'WTD sales', 'WTD Budget',
-                  'WTD Sales Last Year', '+/- vs. last year', 'LBW sales',
-                  'liquor mix', 'foh actual', 'foh target', 'foh variance',
-                  'boh actual', 'boh target', 'boh variance', 'comp 2',
-                  'comp 11', 'comp 6', 'comp 4', 'comp 7', 'comp 8',
-                  'takeaway sales', 'takeaway %', 'deliveries/sales',
-                  'headwait', 'drop variance', 'managers', 'food harvest',
-                  'TCI %', 'TCI variance', 'lunch sales', 'PC drawer'
+                  'liquor_sales', 'beer_sales', 'wine_sales', 'net_sales',
+                  'forecast', 'last_year', 'vs_lastyear' 'WTD_sales', 'WTD_budget',
+                  'WTD_sales_last_year', 'week_vs_lastyear', 'LBW_sales',
+                  'liquor_mix', 'foh_actual', 'foh_target', 'foh_variance',
+                  'boh_actual', 'boh_target', 'boh_variance', 'comp_2',
+                  'comp_11', 'comp_6', 'comp_4', 'comp_7', 'comp_8',
+                  'takeaway_sales', 'takeaway_percentage', 'deliveries_sales',
+                  'headwait', 'drop_variance', 'managers', 'food_harvest',
+                  'TCI_percentage', 'TCI_variance', 'lunch_sales', 'PC_drawer'
                   )
