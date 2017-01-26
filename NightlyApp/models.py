997878fd61240446a28497a97f875a09a05f1fe3
John D. Bower <jdbower0525@gmail.com>
2017-01-25 21:01:40 -0500

    adding sales to models

--- NightlyApp/models.py
+++ NightlyApp/models.py
@@ -7,3 +7,4 @@ class Day(models.Model):
     date = models.DateField(auto_now=False, auto_now_add=False)
     closing_manager = models.CharField(max_length=200)
     headwait = models.CharField(max_length=200)
+    sales = models.IntegerField(default=0)
